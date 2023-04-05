8066e76baf7733bf039cc2860c74917c5b19f6fd
stayuglyq <Samwise2004@yandex.ru>
2023-04-05 21:59:14 +0300

    hello world (version 2)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 int main()
 {
-    cout<<"Hello World";
-
+    std::cout<<"Hello World";
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << "!";
     return 0;
 }
