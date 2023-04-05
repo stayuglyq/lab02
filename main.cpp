00e0841a34e12065679e0cd696e210379a05cc5e
stayuglyq <Samwise2004@yandex.ru>
2023-04-05 22:15:22 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     std::cout<<"Hello World";
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
+    std::string name; //создаю комментарии в исходном коде
+    std::cout << "Enter your name: ";//запрос на ввод имени 
+    std::cin >> name; //ввод имени
     std::cout << "Hello world from " << name << "!";
     //случайно убрал using namespace std; в part 1 
     //"вношу другие изменения"
